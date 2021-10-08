9f42a22f98489ed0f8346f36cc1851918e4bf376
jirenuki-69 <mfuentesmodelo@gmail.com>
2021-10-08 14:31:43 -0500

    jajaja ok willy

--- app/src/main/java/com/example/quizapp/Clases/OptionAdapter.kt
+++ app/src/main/java/com/example/quizapp/Clases/OptionAdapter.kt
@@ -8,13 +8,13 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.example.quizapp.R
 
-class OptionAdapter(private val aContext: Context, private val array: ArrayList<Pareja>) :
-    ArrayAdapter<Pareja>(aContext, R.layout.custom_list_view_item, array) {
+class OptionAdapter(private val mcontext: Context, private val array: ArrayList<Pareja>) :
+    ArrayAdapter<Pareja>(mcontext, R.layout.custom_list_view_item, array) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val text = getItem(position)?.first
 
         val view: View = convertView
-            ?: LayoutInflater.from(aContext)
+            ?: LayoutInflater.from(mcontext)
                 .inflate(R.layout.custom_list_view_item, parent, false)
 
         val textView = view.findViewById<TextView>(R.id.question_option_text)
